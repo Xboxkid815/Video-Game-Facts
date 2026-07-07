82e40e8d4978c5e6f2f70fdb341044e1ebfa89b3
Peyton Smith <s_peyton5@icloud.com>
2026-07-07 14:50:31 -0700

    th

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let count = 0
 function addone() {
     count += 2 ** Math.max(0,Math.floor(Math.log2(count/25)))
+    updateCount();
+}
+
+function updateCount() {
     document.getElementById("counter").innerHTML = count;
 }
