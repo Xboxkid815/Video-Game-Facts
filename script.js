5200aa7aa23a6949a0718bdc3cac775d9b5af47b
Peyton Smith <s_peyton5@icloud.com>
2026-07-08 11:15:06 -0700

    drf

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-let count = 0
-let wood_per_click = 1
-let lumberjacks = 0
 
-let axe_cost = 10
-let lumberjack_cost = 25
-let chainsaw_cost = 100
+let data = {
+    count: 0,
+    wood_per_click: 1,
+    lumberjacks: 0,
+
+    axe_cost: 10,
+    lumberjack_cost: 25,
+    chainsaw_cost: 100,
+
+}
 
 function updateCount() {
-    document.getElementById("counter").innerHTML = count;
-    document.getElementById("axe_cost").innerHTML = axe_cost;
-    document.getElementById("lumberjack_cost").innerHTML = lumberjack_cost;
-    document.getElementById("chainsaw_cost").innerHTML = chainsaw_cost;
+    document.getElementById("counter").innerHTML = data.count;
+    document.getElementById("axe_cost").innerHTML = data.axe_cost;
+    document.getElementById("lumberjack_cost").innerHTML = data.lumberjack_cost;
+    document.getElementById("chainsaw_cost").innerHTML = data.chainsaw_cost;
 
-    if (count >= axe_cost) {
+    if (data.count >= data.axe_cost) {
         document.getElementById("axe").removeAttribute("disabled")
     } else {
         document.getElementById("axe").setAttribute("disabled", "")
     }
-    if (count >= lumberjack_cost) {
+    if (data.count >= data.lumberjack_cost) {
         document.getElementById("lumberjack").removeAttribute("disabled")
     } else {
         document.getElementById("lumberjack").setAttribute("disabled", "")
     }
-    if (count >= chainsaw_cost) {
+    if (data.count >= data.chainsaw_cost) {
         document.getElementById("chainsaw").removeAttribute("disabled")
     } else {
         document.getElementById("chainsaw").setAttribute("disabled", "")
@@ -30,39 +34,39 @@ function updateCount() {
 }
 
 document.getElementById("button").addEventListener("click", function () {
-    count += wood_per_click
+    data.count += data.wood_per_click
     updateCount()
 })
 
 document.getElementById("axe").addEventListener("click", function () {
-    if (count >= axe_cost) {
-        count -= axe_cost
-        axe_cost += 3
+    if (data.count >= data.axe_cost) {
+        data.count -= data.axe_cost
+        data.axe_cost += 3
         updateCount()
-        wood_per_click += 1
+        data.wood_per_click += 1
     }
 })
 
 document.getElementById("lumberjack").addEventListener("click", function () {
-    if (count >= lumberjack_cost) {
-        count -= lumberjack_cost
-        lumberjack_cost += 5
+    if (data.count >= data.lumberjack_cost) {
+        data.count -= data.lumberjack_cost
+        data.lumberjack_cost += 5
         updateCount()
-        lumberjacks += 1
+        data.lumberjacks += 1
     }
 })
 
 document.getElementById("chainsaw").addEventListener("click", function () {
-    if (count >= chainsaw_cost) {
-        count -= chainsaw_cost
-        chainsaw_cost += 10
+    if (data.count >= data.chainsaw_cost) {
+        data.count -= data.chainsaw_cost
+        data.chainsaw_cost += 10
         updateCount()
-        wood_per_click += 10
+        data.wood_per_click += 10
     }
 })
 
 function everySecond() {
-    count += lumberjacks
+    data.count += data.lumberjacks
     updateCount()
     setTimeout(everySecond, 1000)
 }
